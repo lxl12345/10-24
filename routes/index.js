8ccfaf0d6b541dd6f4f4a22518b3e3c685d4973a
lxl12345 <1757593685@qq.com>
2017-10-24 19:00:17 +0800

    李晓璐10月24日作业

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var fs=require('fs');
+/* GET home page. */
+var n=0;
+router.get('/', function(req, res, next) {
+	fs.readFile('read.txt','utf-8',function(err,data){
+		n++
+		console.log(n)
+		fs.writeFile('read.txt','当前浏览的次数是:'+n)
+	})
+  res.render('index', { title: n+1 });
+});
+
+module.exports = router;
